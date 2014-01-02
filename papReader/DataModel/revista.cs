44cc580f11a37025a74ca314cba9fc543a3f9edf
pikax <david-181@hotmail.com>
2014-01-02 15:37:25 +0000

    Trocar leitor local de pdf, e chamar o leitor por defeito do windows. Add ProgressBar no download

--- papReader/DataModel/revista.cs
+++ papReader/DataModel/revista.cs
@@ -26,49 +26,18 @@ namespace papReader.DataModel
 		{
 			try
 			{
-				var xx = new HttpClient();
-				
-				
-				string tex;
-
-				using (var strea = new StreamReader(await xx.GetStreamAsync(url), Encoding.GetEncoding("iso-8859-1")))
+				using (var xx = new HttpClient())
 				{
-					tex = strea.ReadToEnd();
-				}
-				return tex;
-				
+					using (var strea = new StreamReader(await xx.GetStreamAsync(url), Encoding.GetEncoding("iso-8859-1")))
+					{
+						return strea.ReadToEnd();
+					}
+				}	
 			}
 			catch
 			{
 				return null;
 			}
-		}
-
-		static string GetConvertedString(Encoding from, Encoding to, string str)
-		{
-			string unicodeString = str;
-			// Create two different encodings.
-			Encoding ascii = from;
-			Encoding unicode = to;
-
-			// Convert the string into a byte[].
-			byte[] unicodeBytes = unicode.GetBytes(unicodeString);
-
-			// Perform the conversion from one encoding to the other.
-			byte[] asciiBytes = Encoding.Convert(unicode, ascii, unicodeBytes);
-
-			// Convert the new byte[] into a char[] and then into a string.
-			// This is a slightly different approach to converting to illustrate
-			// the use of GetCharCount/GetChars.
-			char[] asciiChars = new char[ascii.GetCharCount(asciiBytes, 0, asciiBytes.Length)];
-			ascii.GetChars(asciiBytes, 0, asciiBytes.Length, asciiChars, 0);
-			string asciiString = new string(asciiChars);
-
-			//// Display the strings created before and after the conversion.
-			//Console.WriteLine("Original string: {0}", unicodeString);
-			//Console.WriteLine("Ascii converted string: {0}", asciiString);
-
-			return asciiString;
 		}
 
 		public static async Task<List<revista>> GetRevistas()
